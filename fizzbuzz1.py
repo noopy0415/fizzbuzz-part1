b4594c2cf4fc55780b19668d5f76bfb055a5de73
noopy0415 <noopy0415@gmail.com>
2018-11-17 17:34:33 +0900

    リファクタリング： outputが邪魔に見えた！

---
+++ fizzbuzz1.py
@@ -0,0 +1,10 @@
+number = int(input("1つの自然数を入れてね: "))
+
+if number % 15 == 0:
+    print("FizzBuzz")
+elif number % 3 == 0:
+    print("Fizz")
+elif number % 5 == 0:
+    print("Buzz")
+else:
+    print(str(number))
